07a64a1da8d2c3423ab0b22b60c6e574bd58c4b9
York_Luo <576716529@qq.com>
2018-04-19 00:48:46 +0800

    增加日志

--- springboot_demo/src/main/java/com/luoy/controller/HelloController.java
+++ springboot_demo/src/main/java/com/luoy/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.luoy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +12,16 @@ import com.luoy.vo.Student;
 @RequestMapping("/hello")
 public class HelloController {
 
+	private final static Logger logger = LoggerFactory.getLogger(com.luoy.controller.HelloController.class);
+	
 	@RequestMapping("/test")
 	public Student print(String str) {
 		Classroom cl=new Classroom("123456","一班");
 		Student student=new Student("23",26,cl);
+		logger.info("日志info");
+		logger.debug("日志debug");
+		logger.error("日志error");
+		
 		return student;
 	}
 }
